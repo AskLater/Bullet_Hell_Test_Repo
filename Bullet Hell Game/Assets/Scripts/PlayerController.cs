743d4b650d07d3534bd241beda42ffb52499b317
Ask Later <agent0098@gmail.com>
2018-05-28 10:30:27 -0500

    Just added a few default assets
    These are just default sprites and backgrounds.  We can change them as soon as the artists have new sprites and backgrounds for us.

---
+++ Bullet Hell Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		var x = Input.GetAxis ("Horizontal");
+		var y = Input.GetAxis ("Vertical");
+
+		transform.Rotate (0, 0, -x);
+		transform.Translate(x, y, 0);
+
+	}
+}
