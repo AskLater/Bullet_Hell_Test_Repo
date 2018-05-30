52e98be94738b8623e120932bd5cb36183f779c4
Ask Later <agent0098@gmail.com>
2018-05-29 20:03:42 -0500

    Added enemies
    All they can do is chase you and they don't fire bullets yet, but we have enemies now!

---
+++ Bullet Hell Game/Assets/Scripts/Enemy1Controller.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1Controller : MonoBehaviour {
+
+	public Transform target;
+	public int moveSpeed;
+	public int rotationSpeed;
+
+	private Transform myTransform;
+
+	void Awake()
+	{
+		myTransform = transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		//GameObject go = GameObject.FindGameObjectWithTag ("Player");
+
+		//target = go.transform;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 dir = target.position - myTransform.position;
+		dir.z = 0.0f;
+			if (dir != Vector3.zero)
+			{
+				myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.FromToRotation(Vector3.right, dir), rotationSpeed * Time.deltaTime);
+			}
+
+		myTransform.position += (target.position - myTransform.position).normalized * moveSpeed * Time.deltaTime;
+
+	}
+
+
+}
